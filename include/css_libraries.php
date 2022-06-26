f9163b9e270192a97b49205f10c3fde3f924bd22
Ghulam Mustafa <ghulammustafa9901@gmail.com>
2022-06-26 14:12:37 +0500

    add favicon

--- include/css_libraries.php
+++ include/css_libraries.php
@@ -1,10 +1,10 @@
-
+  <?php //require 'config.php'; ?>
   <meta charset="utf-8">
-  <title> Tuf Portfolio </title>
-  <!-- <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon" /> -->
-  <!-- <link rel="apple-touch-icon" href="assets/img/favicon.ico" /> -->
-  <!-- <link rel="icon" href="assets/img/favicon.png"> -->
-  <link rel="stylesheet" type="text/css" href="assets/css/all.css">
-  <link rel="stylesheet" type="text/css" href="assets/css/cropper.css">
-  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+  <title> TuF Protfolio </title>
+  <!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> -->
+  <!-- <link rel="apple-touch-icon" href="favicon.ico" /> -->
+  <link rel="icon" href="assets/img/favicon-32x32.png">
+  <link rel="stylesheet" type="text/css" href="assets/css/all.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/cropper.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="assets/css/style.css?v=<?php echo time(); ?>">
--- include/js_libraries.php
+++ include/js_libraries.php
@@ -1,7 +1,7 @@
 
 
-  <script type="text/javascript" src="assets/js/jquery.js"></script>
+  <script type="text/javascript" src="assets/js/jquery.min.js"></script>
   <script type="text/javascript" src="assets/js/popper.v1.12.9.js"></script>
-  <script type="text/javascript" src="assets/js/cropper.js"></script>
-  <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+  <script type="text/javascript" src="assets/js/cropper.min.js"></script>
+  <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="assets/js/main.js?v=<?php echo time(); ?>"></script>
